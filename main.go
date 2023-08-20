9ac444cf1b1639b36065526c36feec52ddcd28a5
ziad amr shalaby <zezoamr@yandex.com>
2023-08-20 18:55:31 +0300

    film struct + templating code in go + basic html/bootstrap
    note: had to download bootstrap and serve it from the server because the cdn kept giving me a cors error that didn't go away even after i allowed all cors :(

--- main.go
+++ main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"net/http"
+	"text/template"
 )
+
+type Film struct {
+	Title    string
+	Director string
+}
 
 func main() {
 
+	http.HandleFunc("/assets/css/bootstrap.min.css", serveCSS)
 	http.HandleFunc("/", rootRoute)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 	fmt.Println("server is running on port 8000")
@@ -16,6 +22,23 @@ func main() {
 }
 
 func rootRoute(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "hello world \n")
-	io.WriteString(w, "some request details "+r.Host+" "+r.Method+" "+r.Proto)
+	//enableCors(&w)
+	films := map[string][]Film{
+		"Films": {
+			{Title: "The Godfather", Director: "Francis Ford Coppola"},
+			{Title: "Blade Runner", Director: "Ridley Scott"},
+			{Title: "The Thing", Director: "John Carpenter"},
+		},
+	}
+	tmpl := template.Must(template.ParseFiles("index.html"))
+	tmpl.Execute(w, films)
+}
+
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+}
+
+func serveCSS(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/css")
+	http.ServeFile(w, r, "assets/css/bootstrap.min.css")
 }
